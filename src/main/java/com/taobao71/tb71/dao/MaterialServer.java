a09316af005f26b396679c659c4f91e48ec2eae1
lyzhang <tianxiamall@163.com>
2021-01-02 21:32:52 +0800

    del code

--- src/main/java/com/taobao71/tb71/dao/MaterialServer.java
+++
@@ -1,9 +0,0 @@
-package com.taobao71.tb71.dao;
-
-import com.taobao71.tb71.domain.Material;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface MaterialServer {
-    public void addMaterial(Material material);
-}
