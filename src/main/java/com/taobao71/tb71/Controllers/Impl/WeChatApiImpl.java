1510388c34cf00054d48fe276625cac199558e9a
lyzhang <tianxiamall@163.com>
2021-01-04 12:02:44 +0800

    add wx test whoami

--- src/main/java/com/taobao71/tb71/Controllers/Impl/WeChatApiImpl.java
+++ src/main/java/com/taobao71/tb71/Controllers/Impl/WeChatApiImpl.java
@@ -1,12 +1,18 @@
 package com.taobao71.tb71.Controllers.Impl;
 
 import com.soecode.wxtools.api.IService;
+import com.soecode.wxtools.api.WxConsts;
+import com.soecode.wxtools.api.WxMessageRouter;
 import com.soecode.wxtools.api.WxService;
 import com.soecode.wxtools.bean.WxXmlMessage;
+import com.soecode.wxtools.bean.WxXmlOutMessage;
 import com.soecode.wxtools.util.xml.XStreamTransformer;
 import com.taobao71.tb71.Controllers.WeChatApi;
+import com.taobao71.tb71.wxHandler.WhoAmIHandler;
+import com.taobao71.tb71.wxMatcher.WhoAmIMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +27,8 @@ import java.io.PrintWriter;
 @RequestMapping("/wxapi")
 public class WeChatApiImpl implements WeChatApi {
 
-  static Logger logger = LoggerFactory.getLogger(WeChatApiImpl.class);
   private IService iService = new WxService();
-
+  static Logger logger = LoggerFactory.getLogger(WeChatApiImpl.class);
 
   @GetMapping
   public String check(String signature, String timestamp, String nonce, String echostr)  {
@@ -45,7 +50,18 @@ public class WeChatApiImpl implements WeChatApi {
 
       // 微信服务器推送过来的是XML格式。
       WxXmlMessage wx = XStreamTransformer.fromXml(WxXmlMessage.class, request.getInputStream());
-      logger.info("消息：\n " + wx.toString());
+      logger.info("消息：{}",wx.toString());
+
+      WxMessageRouter router = new WxMessageRouter(iService);
+
+      router.rule().msgType(
+          WxConsts.XML_MSG_TEXT).matcher(new WhoAmIMatcher()).handler(new WhoAmIHandler()).end();
+      // 把消息传递给路由器进行处理
+      WxXmlOutMessage xmlOutMsg = router.route(wx);
+
+      if(xmlOutMsg != null){
+        out.print(xmlOutMsg.toXml());
+      }
     }catch (IOException  e){
       logger.error("接受到的消息解码失败");
     }catch (Exception e) {
---
+++ src/main/java/com/taobao71/tb71/wxMatcher/WhoAmIMatcher.java
@@ -0,0 +1,20 @@
+package com.taobao71.tb71.wxMatcher;
+
+import com.soecode.wxtools.api.WxMessageMatcher;
+import com.soecode.wxtools.bean.WxXmlMessage;
+import com.soecode.wxtools.util.StringUtils;
+
+public class WhoAmIMatcher implements WxMessageMatcher {
+
+  @Override
+  public boolean match(WxXmlMessage message) {
+
+    if(StringUtils.isNotEmpty(message.getContent())){
+      if(message.getContent().equals("我是谁")){
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
